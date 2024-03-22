d43e06cda26e880cfee67f1e9d7e5be64efb4be6
Vo Anh Vu <vu.voanh@hotmail.com>
2024-03-22 10:30:05 +0700

    init deploy-heroku

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,16 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+# threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+# threads threads_count, threads_count
+
+# preload_app!
+
+# port        ENV.fetch("PORT") { 3000 }
+# environment ENV.fetch("RAILS_ENV") { "development" }
+
+# # Allow puma to be restarted by `rails restart` command.
+# plugin :tmp_restart
+
